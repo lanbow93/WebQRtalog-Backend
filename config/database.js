742cd97b764725b3a08cf5bac13a0891d8c828f2
lanbow93 <the.doctor1501@gmail.com>
2023-12-30 05:25:49 -0600

    linter and prettier installedd, mongo connected

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongo'
+const { MONGODB_URL } = process.env
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+
+mongoose.connection
+  .on('open', () => console.log('Mongoose connected'))
+  .on('close', () => console.log('Disconnected from Mongoose'))
+  .on('error', (error) => console.log(error))
+
+module.exports = mongoose
--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-const app = express();
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+const app = express()
 
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173"
-    ],
-  }),
-);
-app.use(express.json());
-app.use(morgan("common"));
-app.use(cookieParser());
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173']
+  })
+)
+app.use(express.json())
+app.use(morgan('common'))
+app.use(cookieParser())
 
-app.get("/", (request, response) => {
-  response.send("Server is functional");
-});
+app.get('/', (request, response) => {
+  response.send('Server is functional')
+})
 
-const PORT = 7777;
+const PORT = 7777
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+  console.log(`Listening on port: ${PORT}`)
+})
